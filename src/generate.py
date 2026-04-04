16b5c6acc3dac2378793d5cf088b59f769f6f33f
Jamie Lepard <jamie.lepard@gmail.com>
2026-04-03 19:26:43 -0700

    Tempo reduced bt 10%

--- src/generate.py
+++ src/generate.py
@@ -92,7 +92,7 @@ for filename, text in stops.items():
     mp3_path = f"output/{filename}.mp3"
     torchaudio.save(wav_path, wav_tensor, model.sr)
     subprocess.run(
-        ["ffmpeg", "-y", "-i", wav_path, "-codec:audio", "libmp3lame", "-b:a", "192k", mp3_path],
+        ["ffmpeg", "-y", "-i", wav_path, "-filter:a", "atempo=0.9", "-codec:a", "libmp3lame", "-b:a", "192k", mp3_path],
         capture_output=True, check=True
     )
     os.remove(wav_path)
